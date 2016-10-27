097f226bc4e9a277ab5dd2648cc72f0728f769b6
Arthur Gay <Arth-ur@users.noreply.github.com>
2016-10-27 02:12:22 +0200

    remove samples folder

--- samples/charts/load.py
+++
@@ -1,7 +0,0 @@
-import dric
-import dric.support
-
-contentScripts = [
-    {'path': '/content/samples/charts/js/charts.js'}
-]
-dric.register(__name__, dric.support.ContentScriptInjector(contentScripts));
