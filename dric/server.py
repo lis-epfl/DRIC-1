8fc8f28956c2332379068ea146e006663bf0a46c
Arthur Gay <Arth-ur@users.noreply.github.com>
2016-12-13 00:04:48 +0100

    Added CORS to allow all origins :-1:

--- dric/server.py
+++ dric/server.py
@@ -11,6 +11,10 @@ _logger = logging.getLogger('dric.server')
 _eventlet_logger = logging.getLogger('dric.server.eventlet')
 _eventlet_logger.setLevel(logging.INFO)
 
+def funCORS(environ):
+    return [('Access-Control-Allow-Origin', '*')]
+
+
 def _app(environ, start_response):
     # create Request wrapper
     request = Request(environ)
@@ -18,7 +22,11 @@ def _app(environ, start_response):
     # dispatch request, create response
     Response.default_mimetype='text/html'
     response = router.dispatch(request)
-
+    try:
+        response.headers.add('Access-Control-Allow-Origin', '*')
+    except:
+        response.get_headers = funCORS
+	print(type(response))
     return response(environ, start_response)
 
 class Server(object):
