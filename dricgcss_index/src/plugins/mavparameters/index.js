808226d9139e28c46c3b7870c7ec98ae882e62e8
Arthur Gay <Arth-ur@users.noreply.github.com>
2016-12-13 02:01:11 +0100

    Initial commit of mavparameters plugin

---
+++ dricgcss_index/src/plugins/mavparameters/index.js
@@ -0,0 +1,9 @@
+import Parameters from './parameters.vue'
+
+export const routes = [{
+  text: 'Parameters',
+  icon: 'fa fa-sliders',
+  component: Parameters,
+  name: 'mavparameters',
+  path: '/parameters'
+}]
---
+++ dricgcss_index/src/plugins/mavparameters/parameters.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="content body page">
+    <section class="content-header">
+      <h1>
+        Parameters
+      </h1>
+    </section>
+  </section>
+</template>
+<script>
+export default {
+}
+</script>
