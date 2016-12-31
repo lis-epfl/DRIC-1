e8256b7c4847a3e5d9e8cd33284b7e59109ad488
Arthur Gay <Arth-ur@users.noreply.github.com>
2016-12-31 12:10:52 +0100

    FunPass now supports any function attribute name (not only 'pass_info')

--- dric/pass_.py
+++ dric/pass_.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta, abstractmethod
 from future.utils import with_metaclass
 from . import Plugin
+from logging import getLogger
+
+_logger = getLogger('dric.pass_')
 
 class Pass(with_metaclass(ABCMeta)):
     """
@@ -50,21 +53,31 @@ class funpass(object):
 
 class FunPass(Pass):
 
-    def __init__(self, pass_id):
+    def __init__(self, pass_id=None, attr_name='pass_info'):
+        """ 
+        If pass_id is None, all functions with attribute 'attr_name' will be matched.
+        Else, all functions with value 'pass_id' for attribute 'attr_name'.
+        """
         self.pass_id = pass_id
+        self.attr_name = attr_name
 
     def load(self, plugin, name):
+        if not hasattr(self, 'loadfun') or not callable(getattr(self, 'loadfun')):
+            _logger.warn('FunPass does not override member function \'funload\'. Plugin \'{}\' ignored.')
+            return
         # Iterate over all class attributes
         for method_name in dir(plugin):
             method = getattr(plugin, method_name)
             if callable(method):    # only methods
-                if hasattr(method, 'pass_info'):
-                    pass_info = getattr(method, 'pass_info')
-                    if self.pass_id in pass_info:
+                if hasattr(method, self.attr_name):
+                    pass_info = getattr(method, self.attr_name)
+                    if self.pass_id is not None and self.pass_id in pass_info:
                         self.loadfun(plugin, name, method, *pass_info[self.pass_id]['args'], **pass_info[self.pass_id]['kwargs'])
+                    elif self.pass_id is None:
+                        self.loadfun(plugin, name, method, pass_info)
 
-    def loadfun(self, plugin, name, function, *args, **kwargs):
-        pass
+    #def loadfun(self, plugin, name, function, *args, **kwargs):
+    #    pass
 
 def get_pass_manager():
     return _pass_manager
