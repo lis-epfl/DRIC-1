b81891cadcd073a05067b37152a33f504ab07d60
Arthur Gay <Arth-ur@users.noreply.github.com>
2017-01-17 17:31:15 +0100

    Show a warning if dricgcss_index/dist is not found

--- dric/kernel.py
+++ dric/kernel.py
@@ -2,10 +2,14 @@ import SocketServer
 import SimpleHTTPServer
 import threading
 import os
+import os.path
+import logging
 from .pg import start_plugins
 from .ld import load_plugins
 from .server import Server
 from .lockf import LockFileHelper
+
+_logger = logging.getLogger('dric.kernel')
 
 class Kernel(object):
     def __init__(self, ask_user=True):
@@ -20,7 +24,11 @@ class Kernel(object):
         Server('0.0.0.0', 9555).start()
 
     def start_frontend_server(self):
-        os.chdir('./dricgcss_index/dist')
+        frontend_dir = os.path.join('dricgcss_index', 'dist')
+        if not os.path.exists(frontend_dir):
+            _logger.warn('Directory "{}" not found. Frontend server will not be started.'.format(frontend_dir))
+            return
+        os.chdir(frontend_dir)
         Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
         httpd = SocketServer.TCPServer(('', 8080), Handler)
         httpd.serve_forever()
